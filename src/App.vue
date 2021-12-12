86f9b311b2515aca8b6ec2236fe775f9376398b7
Andreas Ernst <andreas.ernst@alvaniev.net>
2021-12-12 19:51:52 +0100

    - images and movies removoed

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,22 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-app app>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <v-app-bar app>
+        <v-toolbar-title class="headline text-uppercase">
+          <span class="font-weight-light">Hockey National League</span>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+      </v-app-bar>
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+      <v-content>
+        <router-view/>
+      </v-content>
+    </v-app>
 </template>
 
 <script>
 
 export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
+  name: 'app',
 };
 </script>
